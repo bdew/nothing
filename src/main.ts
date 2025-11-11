0c9bc6fe1abf62c6add05f7de9cf7c80e57870ee
bdew <bdew@bdew.net>
2025-11-11 20:32:04 +0200

    tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import { run_bg_canvas } from "./bg";
 
-document.querySelector("#msg")!.innerHTML = window.location.hostname.toUpperCase();
+document.querySelector("#msg")!.innerHTML = window.location.hostname;
 
 run_bg_canvas();
